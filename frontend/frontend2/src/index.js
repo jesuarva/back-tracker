c77a67c58ea580ff85a21bd93e8c9aa6eb6ad4e3
jesuarva <jesuarva.developer@gmail.com>
2018-11-16 13:57:15 +0100

    WIRE frontend/index.js with Redux and React-Router

--- frontend/frontend2/src/index.js
+++ frontend/frontend2/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import App from './components/App/App';
 import './index.css';
-import App from './App';
+import reducers from './reducers/index';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware());
+store.dispatch({ type: 'ADD_DATA' });
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
