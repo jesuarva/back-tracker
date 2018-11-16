c6da45652a5c024ff61e2311411a6b99c4273312
jesuarva <jesuarva.developer@gmail.com>
2018-11-16 11:20:42 +0100

    APPLY global middlewares to 'server' in backend/index.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,3 +1,9 @@
 const server = require('express')();
+const setUpGlobalMiddlewares = require('./middlewares').globalMiddlewares;
+
+/**
+ * Apply global middlewares to 'server'
+ */
+setUpGlobalMiddlewares(server);
 
 module.exports = { server };
