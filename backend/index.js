be25e7719db6704de0a7b65c1737d3ff5729b9d4
jesuarva <jesuarva.developer@gmail.com>
2018-11-16 12:34:33 +0100

    WIRE and CONNECT mongoose with backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
 const { server } = require('./server/server');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 7171;
+const { MONGODB_URI } = process.env;
 
-server.listen(port, () => {
-  console.log(`\n*** Listening on port ${port} ***\n`);
-});
+mongoose
+  .connect(
+    MONGODB_URI,
+    { useNewUrlParser: true },
+  )
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('*** CONNECTED to database ***');
+    server.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`*** Listening on port ${port} ***`);
+    });
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.log('*** ERROR connecting to database ***', err);
+  });
