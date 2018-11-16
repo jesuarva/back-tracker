dece06795e5821ba661b7b3f07d5b817e52a1c1b
jesuarva <jesuarva.developer@gmail.com>
2018-11-16 19:47:30 +0100

    ADD more action types to actionTypes.js

--- frontend/frontend2/src/actions/actionTypes.js
+++ frontend/frontend2/src/actions/actionTypes.js
@@ -1,1 +1,9 @@
+// Error
+export const ERROR = 'ERROR';
+
+// main Data.
 export const ADD_DATA = 'ADD_DATA';
+
+// Fetch main data form DB
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const FETCHED_DATA = 'FETCHED_DATA';
