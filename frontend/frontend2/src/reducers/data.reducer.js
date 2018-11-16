9e8fe79137fde159e14b661b4947103509e39792
jesuarva <jesuarva.developer@gmail.com>
2018-11-16 20:02:41 +0100

    UPDATE data.reducer.js to new requirements adding data to store

--- frontend/frontend2/src/reducers/data.reducer.js
+++ frontend/frontend2/src/reducers/data.reducer.js
@@ -3,7 +3,7 @@ import * as A from '../actions/actionTypes';
 export default (state = [], action) => {
   switch (action.type) {
     case A.ADD_DATA:
-      return state.concat(['hola']);
+      return state.concat([action.data]);
     default:
       return state;
   }
