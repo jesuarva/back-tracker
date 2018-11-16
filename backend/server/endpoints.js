9a88b438cda5f3d41b7436bafb33d5b27d1df423
jesuarva <jesuarva.developer@gmail.com>
2018-11-16 12:34:11 +0100

    WIRE person.router.js with server/enpoints.js

--- backend/server/endpoints.js
+++ backend/server/endpoints.js
@@ -1,4 +1,7 @@
+const personRouter = require('../api/routes/person.router');
+
 module.exports = (server) => {
+  server.use('/person', personRouter);
   server.use('/', (req, res) => {
     res.status(200).send('API up and running');
   });
