72f05ca9e70ca636b761d3706dd88fb03fe4d27d
jesuarva <jesuarva.developer@gmail.com>
2018-11-17 13:43:42 +0100

    ADD new Action types

--- frontend/frontend2/src/actions/actionTypes.js
+++ frontend/frontend2/src/actions/actionTypes.js
@@ -7,3 +7,6 @@ export const ADD_DATA = 'ADD_DATA';
 // Fetch main data form DB
 export const FETCHING_DATA = 'FETCHING_DATA';
 export const FETCHED_DATA = 'FETCHED_DATA';
+
+export const ADDING_ITEM = 'ADDING_ITEM';
+export const ADDED_ITEM = 'ADDED_ITEM';
