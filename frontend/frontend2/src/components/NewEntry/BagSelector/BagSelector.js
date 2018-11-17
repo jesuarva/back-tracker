bdcf40a2803b9c579b7bce3fbe81292d918c190e
jesuarva <jesuarva.developer@gmail.com>
2018-11-17 14:26:29 +0100

    IMPLEMENT BagSelector.js component

---
+++ frontend/frontend2/src/components/NewEntry/BagSelector/BagSelector.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+export default class Example extends React.Component {
+  render() {
+    const { value } = this.props;
+    return (
+      <FormGroup>
+        <Label for="bags">Select</Label>
+        <Input type="select" name="bags" id="bags" value={value}>
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup>
+    );
+  }
+}
