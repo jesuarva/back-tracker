7ae9735dedd9dc94ae3ef7f1066e183fde678700
jesuarva <jesuarva.developer@gmail.com>
2018-11-17 13:46:00 +0100

    IMPLEMENT starting structure for Terms.js component

---
+++ frontend/frontend2/src/components/Terms/Terms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const Terms = () => {
+  return (
+    <div>
+      <h1>Pending to be implemented</h1>
+    </div>
+  );
+};
+
+export default withRouter(Terms);
