1cf50fd041f286c4ef598de796ea5b3c4a4dd381
jesuarva <jesuarva.developer@gmail.com>
2018-11-17 14:26:00 +0100

    IMPLEMENT starting new entry component

---
+++ frontend/frontend2/src/components/NewEntry/NewEntry.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Form } from 'reactstrap';
+import BagSelector from './BagSelector/BagSelector';
+import { addingItem } from '../../actions/actionCreators';
+import Inputs from './Inputs/Inputs';
+import Button from './Button/Button';
+
+class NewEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      bags: 0,
+      email: '',
+    };
+  }
+
+  handleClick() {
+    addingItem(this.state);
+  }
+
+  handleChange(event) {
+    event.stopPropagation();
+    console.log(event.target.id, this.state);
+    // this.setState({ [event.target.id]: event.target.value });
+  }
+
+  render() {
+    const { name, email } = this.state;
+
+    return (
+      <Form onChange={this.handleChange}>
+        <Inputs name={name} email={email} />
+        <BagSelector value={this.state.bags} />
+        <Button
+          text="Create new"
+          data={this.state}
+          handleClick={this.handleClick}
+        />
+      </Form>
+    );
+  }
+}
+
+export default connect(() => {
+  return {}, { addingItem };
+})(NewEntry);
