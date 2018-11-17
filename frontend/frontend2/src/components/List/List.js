a27bdc41c23140a4fe7b7593dac6ec3077970798
jesuarva <jesuarva.developer@gmail.com>
2018-11-17 13:44:07 +0100

    DEFINE List.js component

---
+++ frontend/frontend2/src/components/List/List.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ListDetails from '../ListDetails.js/ListDetails';
+
+const List = ({ data }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        {data.map((d, i) => (
+          <ListDetails key={Math.random() + Date.now()} details={d} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStatToProps = (state) => {
+  return {
+    data: state.data,
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStatToProps,
+    {},
+  )(List),
+);
