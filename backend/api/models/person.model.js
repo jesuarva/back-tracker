6a51b2b3082033a9acb7d61a3e0181df023fb82d
jesuarva <jesuarva.developer@gmail.com>
2018-11-17 12:45:59 +0100

    UPDATE model

--- backend/api/models/person.model.js
+++ backend/api/models/person.model.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
-const personSchema = new mongoose.Schema({
+const passengerBagSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "We'll be glad to know your name"],
+    required: [
+      true,
+      'This field is required, We would love to know your name.',
+    ],
+  },
+  bags: {
+    type: Number,
+    required: [true, 'This field is required, How many bags travel with you?'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email address is required'],
+    validate: {
+      validator: (val) =>
+        /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(
+          val,
+        ),
+      message: '{VALUE} is not a valid email address',
+    },
   },
 });
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Passenger-Bags', passengerBagSchema);
